e4b5eddc7f99ceb0517b312d5cf9d009b5132f37
ashnarrainen <133806239+ashnarrainen@users.noreply.github.com>
2024-06-05 00:51:12 +0400

    feat: Init (#1)

--- basic.py
+++ basic.py
@@ -1,4 +1,9 @@
+from qdrant_client import QdrantClient
+from llama_index import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
 from llama_index.llms import Ollama
+from llama_index.storage.storage_context import StorageContext
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+
 
 llm = Ollama(model="mistral", request_timeout=360)
 
