524ed5aebf98d3218659ff504f9d82c8f92bc572
Ashley Narrainen <ashnarrainen@gmail.com>
2024-06-14 21:30:35 +0400

    feat: implement customization tool

--- specialized.py
+++ specialized.py
@@ -4,61 +4,29 @@ from llama_index.llms import Ollama
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
-
-def load_documents():
-    return SimpleDirectoryReader("./data").load_data(True)
-
-
-def create_qdrant_client():
-    return QdrantClient(path="./qdrant_data")
-
-
-def create_vector_store(client):
-    return QdrantVectorStore("TrainedData", client=client)
-
-
-def create_storage_context(vector_store):
-    return StorageContext.from_defaults(vector_store=vector_store)
-
-
-def create_llama():
-    return Ollama(model="mistral", request_timeout=300.0)
-
-
-def create_service_context(llm):
-    return ServiceContext.from_defaults(llm=llm, embed_model="local")
-
-
-def create_index(documents, service_context, storage_context):
-    return VectorStoreIndex.from_documents(
-        documents, service_context=service_context, storage_context=storage_context
-    )
-
-
-def create_query_engine(index):
-    return index.as_query_engine(streaming=True, similarity_top_k=1)
-
-
-def query_data(query_engine, query):
-    return query_engine.query(query)
-
-
-def print_response_stream(response_stream):
-    response_stream.print_response_stream()
+promp = "write me an sql query that gives the top 3 best students for each module in mauritius"
 
 
 def main():
-    documents = load_documents()
-    client = create_qdrant_client()
-    vector_store = create_vector_store(client)
-    storage_context = create_storage_context(vector_store)
-    llm = create_llama()
-    service_context = create_service_context(llm)
-    index = create_index(documents, service_context, storage_context)
-    query_engine = create_query_engine(index)
-    query = "write sql query that gives the top 3 best students for each module in mauritius"
-    response_stream = query_data(query_engine, query)
-    print_response_stream(response_stream)
+    # Input
+    documents = SimpleDirectoryReader("./data").load_data(True)
+
+    qdrant_client = QdrantClient(path="./qdrant_data")
+    qdrant_store = QdrantVectorStore("customized_data", qdrant_client)
+    storage_context = StorageContext.from_defaults(vector_store=qdrant_store)
+
+    # Process
+    llm = Ollama(model="mistral", request_timeout=360)
+    service_context = ServiceContext.from_defaults(llm=llm, embed_model="local")
+    index = VectorStoreIndex.from_documents(
+        documents, service_context=service_context, storage_context=storage_context
+    )
+
+    # Output
+    query_engine = index.as_query_engine(streaming=True)
+    query = promp
+    response_stream = query_engine.query(query)
+    response_stream.print_response_stream()
 
 
 if __name__ == "__main__":
